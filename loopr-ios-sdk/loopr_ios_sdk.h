ec5c091f8f7fe05885eac3ff2e9648b3ab44ae1f
xiaoruby <yuli.xiaodoudou@gmail.com>
2018-05-27 15:22:09 -0700

    Init the project

---
+++ loopr-ios-sdk/loopr_ios_sdk.h
@@ -0,0 +1,19 @@
+//
+//  loopr_ios_sdk.h
+//  loopr-ios-sdk
+//
+//  Created by xiaoruby on 5/27/18.
+//  Copyright © 2018 Loopring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for loopr_ios_sdk.
+FOUNDATION_EXPORT double loopr_ios_sdkVersionNumber;
+
+//! Project version string for loopr_ios_sdk.
+FOUNDATION_EXPORT const unsigned char loopr_ios_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <loopr_ios_sdk/PublicHeader.h>
+
+
